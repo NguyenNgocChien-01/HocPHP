0a69d233f30c3272460bbacaa08d7edd58fa65ed
yourjasmine <skeep1199@gmail.com>
2024-12-16 21:13:28 +0700

    [75] Create Database

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+
+    // kết nối CSDL
     include "connect.php";
+    
+    // câu lệnh tạo CSDL
+    $sql = " CREATE DATABASE CSDL ";
+
+        // chạy thành công tạo ra 1 database mới
+    if(mysqli_query($conn, $sql)){
+        echo "create database successfully";
+    }else{
+        echo "create database failed";
+    }
+
     
 ?>
