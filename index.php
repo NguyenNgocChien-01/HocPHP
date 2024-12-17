3806f2e612124940383bd9358a8c1d204e599d2a
yourjasmine <skeep1199@gmail.com>
2024-12-17 15:22:52 +0700

    [77] - Insert Data

--- index.php
+++ index.php
@@ -2,27 +2,17 @@
     // kết nối CSDL
     include "connect.php";
 
-
-    //  // câu lệnh tạo CSDL
-    // $sql = " CREATE DATABASE dulieuweb ";
-
-
-    // Câu lệnh tạo bảng
-    $sql = "CREATE TABLE thanhvien (
-        id          INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        account     VARCHAR(30) NOT NULL,
-        passwrd     VARCHAR(30) NOT NULL,
-        level       INT(6)
-    )";
-
-    // thực thi truy vấn
-    if($conn->query($sql) == TRUE){
-        echo "Tao bang thanh cong";
-    }else{
-        echo "tao bang khong thanh cong";
-    }
+    $id="";
+    $account= 'admin';
+    $passwrd='123456';
+    $level = 1;
     
-    
-    
+    $sql = "INSERT INTO thanhvien (id, account, passwrd, level) 
+            VALUES('$id', '$account', '$passwrd', '$level');
+    ";
+
+    mysqli_query($conn, $sql);
+
+
 ?>
 
