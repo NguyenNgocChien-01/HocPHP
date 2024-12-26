63347fdd774a9a63d5b8ceb75ed3008634092653
yourjasmine <skeep1199@gmail.com>
2024-12-26 20:23:19 +0700

    select oop

--- duan/index.php
+++ duan/index.php
@@ -12,7 +12,8 @@
         echo "<tr>";
         echo "<td>" . $r['id'] . "</td>";
         echo "<td>" . $r['hoten'] . "</td>";
-        
+        echo "<td>" . $r['username'] . "</td>";
+        echo "<td>" . $r['pass'] . "</td>";
         echo "</tr>";
         echo "<br>";
     }
